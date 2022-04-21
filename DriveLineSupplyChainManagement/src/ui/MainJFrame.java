15a6eb36daeff11a83cdb42297c04b9dae014693
NUsakhyatha <tammineni.y@northeastern.edu>
2022-04-21 03:19:07 -0400

    Changed button

--- DriveLineSupplyChainManagement/src/ui/MainJFrame.java
+++ DriveLineSupplyChainManagement/src/ui/MainJFrame.java
@@ -58,7 +58,11 @@ public class MainJFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+<<<<<<< Updated upstream
         System.out.println("Pressed");
+=======
+        System.out.println("This is not clicked!!");
+>>>>>>> Stashed changes
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
