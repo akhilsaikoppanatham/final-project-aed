f17f8e21344912610c74e4bed3ad6cb687014ee6
Akhil Sai Koppanatham <koppanatham.a@northeastern.edu>
2022-04-23 17:52:19 -0400

    Added getinstance method in db4o

--- DriveLineSupplyChainManagement/src/Bussiness/DB4OUtil/DB4OUtil.java
+++ DriveLineSupplyChainManagement/src/Bussiness/DB4OUtil/DB4OUtil.java
@@ -14,5 +14,11 @@ public class DB4OUtil {
 
     private static final String FILENAME = Paths.get("Databank.db4o").toAbsolutePath().toString();// path to the data store
     private static DB4OUtil dB4OUtil;
+    public synchronized static DB4OUtil getInstance(){
+        if (dB4OUtil == null){
+            dB4OUtil = new DB4OUtil();
+        }
+        return dB4OUtil;
+    }
     
 }
