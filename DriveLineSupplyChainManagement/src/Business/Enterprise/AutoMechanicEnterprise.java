78c74a91b27783bf18f5d0aeeb0c33609280dc4a
Nikita reddy Dalli <dalli.n@northeastern.edu>
2022-04-23 13:08:46 -0400

    Made changes to AutoMechanicEnterprise

--- DriveLineSupplyChainManagement/src/Business/Enterprise/AutoMechanicEnterprise.java
+++ DriveLineSupplyChainManagement/src/Business/Enterprise/AutoMechanicEnterprise.java
@@ -4,10 +4,51 @@
  */
 package Business.Enterprise;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author nikki
  */
-public class AutoMechanicEnterprise {
+public class AutoMechanicEnterprise extends Enterprise {
+
+    private String fix;
+    private int automobileNumber;
+    private String fixType;
+    private int minimumThreshold;
+    private int maximumThreshold;
+    private ArrayList<String> testsList;
+    public AutoMechanicEnterprise(String name) {
+        super(name, Enterprise.EnterpriseType.AutoMechanicEnterprise);
+        this.testsList = new ArrayList<String>();
+    }
+
+    public void createDose(String dose) {
+
+        this.testsList.add(dose);
+    }
+
+    public String getFix() {
+        return fix;
+    }
+
+    public void setFix(String fix) {
+        this.fix = fix;
+    }
+
+    public int getAutomobileNumber() {
+        return automobileNumber;
+    }
+
+    public void setAutomobileNumber(int automobileNumber) {
+        this.automobileNumber = automobileNumber;
+    }
+    public String getFixType() {
+        return fixType;
+    }
+
+    public void setFixType(String fixType) {
+        this.fixType = fixType;
+    }
+
     
-}
