57d9ec6a23c9db9ebbc2d99bbc3b14f50a83d6f8
Akhil Sai Koppanatham <koppanatham.a@northeastern.edu>
2022-04-23 20:24:23 -0400

    Create NewClass.java

---
+++ DriveLineSupplyChainManagement/src/Bussiness/Role/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bussiness.Role;
+
+import Business.EcoSystem;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author akhilsaikoppanatham
+ */
+public class SalesManagerRole extends Role {
+    @Override
+    public JPanel createWorkArea(JPanel rightPanel, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new SalesManagerWorkAreaJPanel(rightPanel, enterprise, account, business);
+    }
+
+}
