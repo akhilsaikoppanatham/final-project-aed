2dd9045fb4810853cca3d9b418664950d856f180
Akhil Sai Koppanatham <koppanatham.a@northeastern.edu>
2022-04-23 21:28:27 -0400

    Create UserAccountDirectory.java

---
+++ DriveLineSupplyChainManagement/src/Business/UserAccount/UserAccountDirectory.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.UserAccount;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author akhilsaikoppanatham
+ */
+public class UserAccountDirectory {
+    
+    private ArrayList<UserAccount> userAccountList;
+
+    public UserAccountDirectory() {
+        userAccountList = new ArrayList();
+    }
+
+    public ArrayList<UserAccount> getUserAccountList() {
+        return userAccountList;
+    }
+    
+    public UserAccount authenticateUser(String username, String password){
+        for (UserAccount ua : userAccountList)
+            if (ua.getUsername().equals(username) && ua.getPassword().equals(password)){
+                return ua;
+            }
+        return null;
+    }
+    
+   
+}
+
