9b27979ee9a646883a2a8adf0fc5ee22c2aba4ff
Akhil Sai Koppanatham <koppanatham.a@northeastern.edu>
2022-04-23 21:50:39 -0400

    Update GarageWorkRequest.java

--- DriveLineSupplyChainManagement/src/Business/WorkQueue/GarageWorkRequest.java
+++ DriveLineSupplyChainManagement/src/Business/WorkQueue/GarageWorkRequest.java
@@ -9,5 +9,26 @@ package Business.WorkQueue;
  * @author akhilsaikoppanatham
  */
 public class GarageWorkRequest {
-    
+
+    private String testResult;
+    private String automobileName;
+    private String testName;
+    private String fileupload;
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getFileupload() {
+        return fileupload;
+    }
+
+    public void setFileupload(String fileupload) {
+        this.fileupload = fileupload;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
 }
