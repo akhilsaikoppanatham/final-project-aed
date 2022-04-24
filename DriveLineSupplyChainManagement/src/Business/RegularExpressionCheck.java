a59fb86278e943d8c4fba489e4e836d13fdc3999
Akhil Sai Koppanatham <koppanatham.a@northeastern.edu>
2022-04-23 21:00:08 -0400

    Update RegularExpressionCheck.java

--- DriveLineSupplyChainManagement/src/Business/RegularExpressionCheck.java
+++ DriveLineSupplyChainManagement/src/Business/RegularExpressionCheck.java
@@ -4,10 +4,24 @@
  */
 package Business;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author yojanitammineni
  */
 public class RegularExpressionCheck {
-    
+
+    public static int c = 0;
+
+    public static boolean findRE(String userName) {
+        Pattern ptr = Pattern.compile("^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$", Pattern.CASE_INSENSITIVE);
+        Matcher m = ptr.matcher(userName);
+        //          if(m.find())
+//            return true;
+//          else 
+//              return false;
+        return true;
+    }
 }
