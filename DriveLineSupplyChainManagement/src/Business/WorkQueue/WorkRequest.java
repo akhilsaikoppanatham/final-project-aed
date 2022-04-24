4c4acb19b12240e1d82f7433dbc43149de791dc2
Akhil Sai Koppanatham <koppanatham.a@northeastern.edu>
2022-04-23 22:52:56 -0400

    Update WorkRequest.java

--- DriveLineSupplyChainManagement/src/Business/WorkQueue/WorkRequest.java
+++ DriveLineSupplyChainManagement/src/Business/WorkQueue/WorkRequest.java
@@ -27,7 +27,7 @@ public class WorkRequest {
     public String getMessage() {
         return message;
     }
-
+    
     public void setMessage(String message) {
         this.message = message;
     }
