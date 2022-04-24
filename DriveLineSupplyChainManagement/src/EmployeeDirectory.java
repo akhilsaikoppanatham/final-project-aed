bc6a4d24fab3d6851580429c920382d8e9d814e2
NUsakhyatha <tammineni.y@northeastern.edu>
2022-04-24 13:31:12 -0400

    Update EmployeeDirectory.java

--- DriveLineSupplyChainManagement/src/EmployeeDirectory.java
+++ DriveLineSupplyChainManagement/src/EmployeeDirectory.java
@@ -1,3 +1,7 @@
+
+import Business.Employee.Employee;
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +12,14 @@
  * @author yojanitammineni
  */
 public class EmployeeDirectory {
+    private ArrayList<Employee> employeeList;
+
+    public EmployeeDirectory() {
+        employeeList = new ArrayList();
+    }
+
+    public ArrayList<Employee> getEmployeeList() {
+        return employeeList;
+    }
     
 }
