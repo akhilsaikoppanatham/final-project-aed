60c51e20373ad9b838103b619ec33009f2e35e81
NUsakhyatha <tammineni.y@northeastern.edu>
2022-04-26 19:48:11 -0400

    Update AutomobileDirectory.java

--- DriveLineSupplyChainManagement/src/Business/Automobile/AutomobileDirectory.java
+++ DriveLineSupplyChainManagement/src/Business/Automobile/AutomobileDirectory.java
@@ -4,10 +4,39 @@
  */
 package Business.Automobile;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author yojanitammineni
  */
 public class AutomobileDirectory {
     
+    private ArrayList<Automobile> automobilesList;
+
+    public AutomobileDirectory() {
+        automobilesList = new ArrayList();
+    }
+
+    public ArrayList<Automobile> getAutomobileList() {
+        return automobilesList;
+    }
+     public Automobile createAutomobile(String automobileName, int automobileAge, String fuelType, String automobileClass, String contactNum, String address, String sign,
+            String emailAdd, String ownerName, String ownerContact) {
+        Automobile automobile = new Automobile();
+        automobile.setName(automobileName);
+        automobile.setAge(automobileAge);
+        automobile.setFuelType(fuelType);
+        automobile.setAutomobileType(automobileClass);
+        automobile.setContactNum(contactNum);
+        automobile.setAddress(address);
+        automobile.setSign(sign);
+        automobile.setEmailAdd(emailAdd);
+        automobile.setOwnerName(ownerName);
+        automobile.setOwnerContact(ownerContact);
+        automobilesList.add(automobile);
+        return automobile;
+    
+}
 }
