bf03c00ac5688f383dcd56ad4b84f5cd07d01686
Nikita reddy Dalli <dalli.n@northeastern.edu>
2022-04-26 19:36:04 -0400

    Create Network.java

---
+++ DriveLineSupplyChainManagement/src/Business/Network/Network.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Network;
+import Business.Enterprise.EnterpriseDirectory;
+
+/**
+ *
+ * @author nikki
+ */
+public class Network {
+    
+}
