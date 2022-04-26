2d5395a6cfd718b86eb24607d64c688cbd438c55
Nikita reddy Dalli <dalli.n@northeastern.edu>
2022-04-26 17:41:28 -0400

    Create SalesEnterprise.java

---
+++ DriveLineSupplyChainManagement/src/Business/Enterprise/SalesEnterprise.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Enterprise;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author nikki
+ */
+public class SalesEnterprise {
+    
+}
