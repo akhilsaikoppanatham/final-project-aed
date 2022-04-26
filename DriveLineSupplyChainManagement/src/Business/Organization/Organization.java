aad3da75480a511943752cf783cb970246b7752a
Nikita reddy Dalli <dalli.n@northeastern.edu>
2022-04-26 00:30:44 -0400

    Create Organization.java

---
+++ DriveLineSupplyChainManagement/src/Business/Organization/Organization.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Organization;
+import Business.Employee.EmployeeDirectory;
+import Business.Role.Role;
+import Business.UserAccount.UserAccountDirectory;
+import Business.Automobile.AutomobileDirectory;
+import Business.WorkQueue.WorkQueue;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nikki
+ */
+public class Organization {
+    
+}
