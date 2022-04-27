f37225963d5e7f3b5adf2092fa59f52fe78c3a7c
Nikita reddy Dalli <dalli.n@northeastern.edu>
2022-04-26 20:26:19 -0400

    Create InspectionOrganization.java

---
+++ DriveLineSupplyChainManagement/src/Business/Organization/InspectionOrganization.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Organization;
+import Business.Role.GarageMechanicRole;
+import Business.Role.Role;
+import Business.Role.InspectionMechanicRole;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nikki
+ */
+public class InspectionOrganization {
+    
+}
