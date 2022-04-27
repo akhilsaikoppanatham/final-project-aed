df23d7327d0b912d7023962523f9ce3fb65daee0
NUsakhyatha <tammineni.y@northeastern.edu>
2022-04-26 23:12:59 -0400

    Update Specifications.java

--- DriveLineSupplyChainManagement/src/Business/Automobile/Specifications.java
+++ DriveLineSupplyChainManagement/src/Business/Automobile/Specifications.java
@@ -4,10 +4,38 @@
  */
 package Business.Automobile;
 
+import java.util.Date;
+
 /**
  *
  * @author yojanitammineni
  */
 public class Specifications {
+    private int mileage;
+    private int engineRPM;
+    private int ignitionLevel;
+    private float automobileWeight;
+    private int bodyLength;
+    private Date time;
+
+    public Specifications() {
+        
+    }
+     public int getBodyLength() {
+        return bodyLength;
+    }
+
+    public void setBodyLength(int bodyLength) {
+        this.bodyLength = bodyLength;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
     
 }
