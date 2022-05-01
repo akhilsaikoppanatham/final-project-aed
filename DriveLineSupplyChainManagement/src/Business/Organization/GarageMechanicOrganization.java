425ae580e4951f601d058ffe7e1a60bf400639cc
NUsakhyatha <tammineni.y@northeastern.edu>
2022-05-01 09:59:12 -0400

    Update GarageMechanicOrganization.java

--- DriveLineSupplyChainManagement/src/Business/Organization/GarageMechanicOrganization.java
+++ DriveLineSupplyChainManagement/src/Business/Organization/GarageMechanicOrganization.java
@@ -19,7 +19,7 @@ public class GarageMechanicOrganization extends Organization{
         super(Type.GarageMechanicOrganization.getValue());
     }
     
-    @Override
+    @Overridee
     public ArrayList<Role> getSupportedRole() {
         ArrayList<Role> roles = new ArrayList();
         roles.add(new GarageMechanicRole());
